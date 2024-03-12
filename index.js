d9771539e7021d19567580e2e7415f2855a29c65
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2024-03-12 15:22:10 +0300

    fix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 // Cross-Origin Resource Sharing
-app.use(cors({origin: [ 'http://localhost:3000', 'https://car-ports-client.vercel.app'], credentials: true}))
+app.use(cors({origin: [ 'http://localhost:3000', 'https://carports-lk.pro'], credentials: true}))
 // middleware для работы с json
 app.use(express.json())
 // middleware для статики (img, css)
@@ -36,7 +36,7 @@ app.use('/api', router)
 app.use(errorMiddleware)
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin',  'http://localhost:3000', 'https://car-ports-client.vercel.app' );
+    res.setHeader('Access-Control-Allow-Origin',  'http://localhost:3000', 'https://carports-lk.pro' );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
