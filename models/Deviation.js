eab4f0366e4e8268375a7b94d478ddb76f674751
Kirill Shein <kshein71@yandex.ru>
2026-04-20 16:21:52 +0300

    fix

--- models/Deviation.js
+++ models/Deviation.js
@@ -4,13 +4,36 @@ import {Deviation as DeviationMapping} from './mapping.js'
 class Deviation {
 
     async create(data) {
-        const {device_id, deviation_x_mm, deviation_y_mm, internal_temperature_celsius, wifi_signal_strength_dbm, firmware_version, uptime_seconds, error_code, error_message, time, measurement_timestamp} = data
+        const {
+            device_id, 
+            deviation_x_mm, 
+            deviation_y_mm, 
+            internal_temperature_celsius, 
+            wifi_signal_strength_dbm, 
+            firmware_version, 
+            uptime_seconds, 
+            error_code, 
+            error_message, 
+            measurement_timestamp  // ← убрал 'time'
+        } = data;
           
-        const devation = await DeviationMapping.create({device_id, deviation_x_mm, deviation_y_mm, internal_temperature_celsius, wifi_signal_strength_dbm, firmware_version, uptime_seconds, error_code, error_message, time, measurement_timestamp})
+        const deviation = await DeviationMapping.create({
+            device_id, 
+            deviation_x_mm, 
+            deviation_y_mm, 
+            internal_temperature_celsius, 
+            wifi_signal_strength_dbm, 
+            firmware_version, 
+            uptime_seconds, 
+            error_code, 
+            error_message, 
+            measurement_timestamp
+        });
             
-        const created = await DeviationMapping.findByPk(devation.id) 
-        return created
+        const created = await DeviationMapping.findByPk(deviation.id);
+        return created;
     }
+
 }
 
 export default new Deviation()
